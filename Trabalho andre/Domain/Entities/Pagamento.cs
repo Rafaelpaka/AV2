b75b22dcf8c4eae7dd89b4c48ef246fe79005f03
Rafaelpaka <rafael24un@gmail.com>
2025-11-20 16:37:07 -0300

    Delete Trabalho andre directory

--- Trabalho andre/Domain/Entities/Pagamento.cs
+++
@@ -1,9 +0,0 @@
-namespace Domain.Entities
-{
-    public class Pagamento
-    {
-        public int IdPagamento { get; set; }
-        public decimal Valor { get; set; }
-        public string Status { get; set; }
-    }
-}
